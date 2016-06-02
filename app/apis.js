1ece8cf6314e1c2fea140d041260c22759d0dabf
Praseetha-KR <voidimagineer@yahoo.com>
2016-06-02 21:50:42 +0530

    Add UserShowFactory factory in tickApis

---
+++ app/apis.js
@@ -0,0 +1,18 @@
+angular.module('twickApis', [])
+
+.factory('UserShowFactory', [
+    '$resource',
+    function($resource) {
+        var userResource = $resource('https://api.twitter.com/1.1/users/show.json', null, null, {
+            stripTrailingSlashes: false
+        });
+
+        return {
+            get: function(screen_name) {
+                return userResource.get({
+                    screen_name: screen_name
+                }).$promise;
+            }
+        };
+    }
+]);
